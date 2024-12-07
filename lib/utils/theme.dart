b2a944f8030bc03eb2d6bf16574922d80fbc3919
Sachin Patadiya <22010101142@darshan.ac.in>
2024-12-07 13:08:19 +0530

    add util folder

---
+++ lib/utils/theme.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+
+// class ColorName{
+//   Color get grey => Colors.blueGrey.shade600;
+//   Color get white=>Colors.white;
+//   Color get lightgrey=>Colors.grey.shade200;
+//   Color get black => Colors.black;
+
+//   Color get lightWhite=>Colors.white54;
+//   Color get orange=>Colors.deepOrange;
+//   Color get myBlue => Color.fromRGBO(81, 86, 253, 1.0);
+//   Color get textColor => Color.fromRGBO(223, 245, 255, 1);
+//   Color get whiteColor => Colors.white;
+//   Color kPrimaryBackgroundColor =Colors.indigo;
+// }
